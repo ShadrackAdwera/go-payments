9ff9a392e0329b69539ff987fab579ce5f148ea6
ShadrackAdwera <adweshshaddie@gmail.com>
2023-03-20 15:57:28 +0300

    modify schema

--- db/migrations/000001_init_db.up.sql
+++ db/migrations/000001_init_db.up.sql
@@ -1,3 +1,15 @@
+CREATE TYPE "payment_types" AS ENUM (
+  'master_card',
+  'visa',
+  'mpesa'
+);
+
+CREATE TYPE "approval_status" AS ENUM (
+  'pending',
+  'approved',
+  'rejected'
+);
+
 CREATE TABLE "users" (
   "id" uuid PRIMARY KEY,
   "username" varchar NOT NULL,
@@ -11,15 +23,15 @@ CREATE TABLE "clients" (
   "email" varchar UNIQUE NOT NULL,
   "phone" varchar UNIQUE NOT NULL,
   "account_number" varchar,
-  "preferred_payment_type" varchar UNIQUE NOT NULL
+  "preferred_payment_type" payment_types NOT NULL
 );
 
 CREATE TABLE "requests" (
   "id" bigserial PRIMARY KEY,
   "title" varchar NOT NULL,
-  "status" varchar NOT NULL DEFAULT 'PENDING',
+  "status" approval_status NOT NULL,
   "amount" bigint NOT NULL,
-  "paid_to" bigint,
+  "paid_to_id" bigint NOT NULL,
   "createdby_id" uuid NOT NULL,
   "approvedby_id" uuid NOT NULL,
   "created_at" timestamptz NOT NULL DEFAULT (now()),
@@ -39,7 +51,7 @@ CREATE UNIQUE INDEX ON "user_payments" ("client_id", "request_id");
 
 COMMENT ON COLUMN "requests"."status" IS 'Payment Status can be PENDING or RESOLVED';
 
-ALTER TABLE "requests" ADD FOREIGN KEY ("paid_to") REFERENCES "clients" ("id");
+ALTER TABLE "requests" ADD FOREIGN KEY ("paid_to_id") REFERENCES "clients" ("id");
 
 ALTER TABLE "requests" ADD FOREIGN KEY ("createdby_id") REFERENCES "users" ("id");
 
