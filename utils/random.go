5d2132368d0084d7025b6886d204d5e65b4799b2
ShadrackAdwera <adweshshaddie@gmail.com>
2023-03-22 08:48:46 +0300

    create random data utility

---
+++ utils/random.go
@@ -0,0 +1,34 @@
+package utils
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabet = "qazwsxedcrfvtgbyhnujmikolp"
+
+func init() {
+	rand.New(rand.NewSource(time.Now().UnixNano()))
+}
+
+func RandomString(n int) string {
+	var sb strings.Builder
+
+	k := len(alphabet)
+
+	for i := 0; i < n; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+	return sb.String()
+}
+
+func RandomInteger(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+func RandomPreferredPayment() string {
+	status := []string{"master_card", "visa", "mpesa"}
+	return status[rand.Intn(len(status))]
+}
