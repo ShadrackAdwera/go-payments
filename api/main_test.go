7f2ea87d0084005d8375099af76e4d9b08553cc6
ShadrackAdwera <adweshshaddie@gmail.com>
2023-03-28 09:19:57 +0300

    set up main test for mock server

---
+++ api/main_test.go
@@ -0,0 +1,19 @@
+package api
+
+import (
+	"os"
+	"testing"
+
+	db "github.com/ShadrackAdwera/go-payments/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+func newServer(store db.TxStore) *Server {
+	srv := NewServer(store)
+	return srv
+}
+
+func TestMain(m *testing.M) {
+	gin.SetMode(gin.TestMode)
+	os.Exit(m.Run())
+}
