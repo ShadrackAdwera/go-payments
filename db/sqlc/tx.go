067239247a48256a189e665fd78beb03b80075d4
ShadrackAdwera <adweshshaddie@gmail.com>
2023-03-28 08:31:28 +0300

    create reusable tx func

---
+++ db/sqlc/tx.go
@@ -0,0 +1,44 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+)
+
+type TxStore interface {
+	Querier
+	ApproveRequestTx(ctx context.Context, args ApproveRequestTxRequest) (ApproveRequestTxResponse, error)
+}
+
+type Store struct {
+	*Queries
+	db *sql.DB
+}
+
+func NewStore(db *sql.DB) TxStore {
+	return &Store{
+		db:      db,
+		Queries: New(db),
+	}
+}
+
+func (s *Store) execTx(context context.Context, fn func(*Queries) error) error {
+	tx, err := s.db.BeginTx(context, &sql.TxOptions{
+		Isolation: sql.LevelReadCommitted,
+	})
+
+	if err != nil {
+		return err
+	}
+
+	q := New(tx)
+	err = fn(q)
+
+	if err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			return rbErr
+		}
+		return err
+	}
+	return tx.Commit()
+}
