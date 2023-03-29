124e9d2e096b41e7d7f2d714775c1b8b342a5de4
ShadrackAdwera <adweshshaddie@gmail.com>
2023-03-29 15:38:50 +0300

    modify home handler to show user profile

---
+++ api/home_handler.go
@@ -0,0 +1,41 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-gonic/gin"
+	"github.com/mitchellh/mapstructure"
+)
+
+type Profile struct {
+	Iss      string   `json:"iss"`
+	Sub      string   `json:"sub"`
+	Aud      []string `json:"aud"`
+	Iat      int64    `json:"iat"`
+	Exp      int64    `json:"exp"`
+	Azp      string   `json:"azp"`
+	Scope    string   `json:"scope"`
+	Name     string   `json:"name"`
+	Nickname string   `json:"nickname"`
+	Picture  string   `json:"picture"`
+	Sid      string   `json:"sid"`
+}
+
+func (s *Server) home(ctx *gin.Context) {
+	var profileData Profile
+	session := sessions.Default(ctx)
+	profile := session.Get("profile")
+
+	if profile == nil {
+		ctx.JSON(http.StatusAccepted, gin.H{"message": "please login to proceed"})
+		return
+	}
+
+	mapstructure.Decode(profile, &profileData)
+
+	fmt.Println(profileData)
+
+	ctx.JSON(http.StatusAccepted, gin.H{"message": fmt.Sprintf("welcome %s", profileData.Nickname)})
+}
