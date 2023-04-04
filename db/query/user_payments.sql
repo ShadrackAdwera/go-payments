ca1f817f65266197e773f66e749178420a1b9446
ShadrackAdwera <adweshshaddie@gmail.com>
2023-04-04 09:46:59 +0300

    modify get user payment query

--- db/query/user_payments.sql
+++ db/query/user_payments.sql
@@ -7,12 +7,18 @@ INSERT INTO user_payments (
 RETURNING *;
 
 -- name: GetUserPayment :one
-SELECT * FROM user_payments
-WHERE id = $1 LIMIT 1;
+SELECT user_payments.id, user_payments.client_id, user_payments.request_id, clients.name, clients.preferred_payment_type, requests.title, requests.amount, requests.status 
+FROM user_payments
+JOIN clients ON user_payments.client_id = clients.id
+JOIN requests ON user_payments.request_id = requests.id
+WHERE user_payments.id = $1 LIMIT 1;
 
 -- name: GetUserPayments :many
-SELECT * FROM user_payments 
-ORDER BY id
+SELECT user_payments.id, user_payments.client_id, user_payments.request_id, clients.name, clients.preferred_payment_type, requests.title, requests.amount, requests.status 
+FROM user_payments
+JOIN clients ON user_payments.client_id = clients.id
+JOIN requests ON user_payments.request_id = requests.id
+ORDER BY user_payments.id
 LIMIT $1
 OFFSET $2;
 
