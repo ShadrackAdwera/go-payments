b0c6e39fc047c2e5eb532a5f8074c07794ac70dd
ShadrackAdwera <adweshshaddie@gmail.com>
2023-04-06 09:52:43 +0300

    add more permissions

--- utils/permission_names.go
+++ utils/permission_names.go
@@ -1,9 +1,20 @@
 package utils
 
 const (
-	UsersCreate      = "users:create"
-	PaymentInitiator = "payment:initiator"
-	PaymentApprover  = "payment:approver"
-	RequestsRead     = "requests:read"
-	RequestsApprove  = "requests:approve"
+	UsersCreate       = "users:create"
+	UsersRead         = "users:read"
+	UsersDelete       = "users:delete"
+	ClientsRead       = "clients:read"
+	ClientsCreate     = "clients:create"
+	PermissionsRead   = "permissions:read"
+	PermissionsCreate = "permissions:create"
+	PaymentInitiator  = "payment:initiator"
+	PaymentApprover   = "payment:approver"
+	RequestsRead      = "requests:read"
+	RequestsApprove   = "requests:approve"
 )
+
+type PermissionData struct {
+	PermissionName        string `json:"permission_name"`
+	PermissionDescription string `json:"permission_description"`
+}
